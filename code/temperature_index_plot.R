550089fdcfe05f21da54a4759eb1d91538d71b6e
Stephen Minster <steve@Mac.lan>
2025-03-10 08:51:21 -0400

    I hope this works

--- code/temperature_index_plot.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-data_url <-   "https://data.giss.nasa.gov/gistemp/tabledata_v4/GLB.Ts+dSST.csv"
-
-read_csv(data_url, skip=1, na="***")
-
-read_csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>% 
-  select(year = Year, t_diff = `J-D`, )
-
